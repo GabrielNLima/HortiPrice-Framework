9fe0f3b8da1a84ee886ba281878ac308ba975ae9
Gabriel <127705796+GabrielNLima@users.noreply.github.com>
2025-05-23 16:08:49 -0300

    First Commit

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/info', function () {
+    return response()->json([
+        'version' => config('app.version'),
+        'url' => config('app.url'),
+        'env' => config('app.env'),
+    ]);
+});
