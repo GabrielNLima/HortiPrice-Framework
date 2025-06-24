5689bdb97ad87bcf7cc7d8f4d982cc7ae8582e7c
Gabriel <127705796+GabrielNLima@users.noreply.github.com>
2025-06-24 15:04:52 -0300

    Fix

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    protected $table = 'categoria'; // Nome da tabela real
+    protected $table = 'categoria';
 
     const CATEGORIA_DESCRICAO = 'categoria_descricao';
 
